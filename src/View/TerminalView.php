ad667cd904023d7af6b6e732ef952b81fab9f6e6
Quentin Richert <quentin@quentinrichert.com>
2019-12-02 21:48:54 +0100

    Made resources's paths absolute && explicitly exlcuded CSS and JS files from rewrite in .htaccess

--- src/View/TerminalView.php
+++ src/View/TerminalView.php
@@ -4,17 +4,17 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<link rel="stylesheet" type="text/css" href="css/root.css">
-		<link rel="stylesheet" type="text/css" href="css/reset.css">
-		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<link rel="stylesheet" type="text/css" href="css/lib/Goji/terminal.css">
+		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/root.css">
+		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/reset.css">
+		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/main.css">
+		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/lib/Goji/terminal.css">
 		<title>Terminal</title>
 	</head>
 	<body>
 		<div id="terminal" data-action="<?= WEBROOT; ?>/xhr"></div>
 
-		<script src="js/lib/goji/SimpleRequest.class.min.js"></script>
-		<script src="js/lib/goji/Terminal.class.min.js"></script>
+		<script src="<?= WEBROOT; ?>/js/lib/Goji/SimpleRequest.class.min.js"></script>
+		<script src="<?= WEBROOT; ?>/js/lib/Goji/Terminal.class.min.js"></script>
 		<script>
 			(function () {
 				new Terminal(document.querySelector('#terminal'))
