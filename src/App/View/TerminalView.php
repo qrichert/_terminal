ccc03dc10ef718300e5f30e97c3bef6055b7cdb6
Quentin Richert <quentin@quentinrichert.com>
2020-01-03 11:46:21 +0100

    Added favicon

--- src/App/View/TerminalView.php
+++ src/App/View/TerminalView.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta charset="utf-8">
+		<title>Terminal</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+		<link rel="shortcut icon" href="<?= WEBROOT; ?>/img/favicon/favicon--medium--256x256.png"> <!-- Favicon -->
+		<link rel="apple-touch-icon" sizes="512x512" href="<?= WEBROOT; ?>/img/favicon/favicon--large--512x512.png"> <!-- Safari favorite -->
+		<link rel="mask-icon" href="<?= WEBROOT; ?>/img/favicon/bookmark-icon.svg" color="#060607"> <!-- Safari bookmark -->
+
 		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/root.css">
 		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/reset.css">
 		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/main.css">
 		<link rel="stylesheet" type="text/css" href="<?= WEBROOT; ?>/css/terminal.css">
-		<title>Terminal</title>
 	</head>
 	<body>
 		<div id="terminal" data-action="<?= WEBROOT; ?>/xhr"></div>
